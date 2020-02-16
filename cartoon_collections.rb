1510d8bd4d28f55bc584c31bbab466f6d58df9b1
Jacky Lin <jacky.jia.lin@gmail.com>
2020-02-16 06:39:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)  # code an argument here
+  list = []
+  index = 1
+  dwarves.each do | dwarves | 
+    list.push ("#{index} #{dwarves}")
+  index += 1   
+end 
+puts list 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  list = []
+  index = 0 
+  veggies.each do | x | 
+    list.push ("#{x.capitalize}!")
+  index += 1   
+end 
+list 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(short_words)
+  short_words.any? do | x | 
+    x.length > 4 
+  end 
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
